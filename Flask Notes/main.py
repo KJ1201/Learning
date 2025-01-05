964aeb6d37c235093021410d8af6d32f8605c216
KJ1201 <karanjuneja2006@outlook.com>
2025-01-05 22:54:26 +0530

    Flask Notes

---
+++ Flask Notes/main.py
@@ -0,0 +1,12 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+'''
+Anything related to delete function and javascript is not working
+So, handle it accordingly
+'''
